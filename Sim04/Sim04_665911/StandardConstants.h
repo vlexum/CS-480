641573db3900074a4023c26a3ded887152bbc30d
vlexum <scottyaustin5@gmail.com>
2022-12-05 12:27:02 -0700

    sim 4

---
+++ Sim04/Sim04_665911/StandardConstants.h
@@ -0,0 +1,49 @@
+// Preprocessor directives
+#ifndef STANDARD_CONSTANTS_H
+#define STANDARD_CONSTANTS_H
+
+// global constants
+
+// constant for end of c-string null character
+#define CARRIAGE_RETURN_CHAR '\r'
+
+//constant for a COLON
+#define COLON ':'
+
+//constant for a COMMA
+#define COMMA ','
+
+//Constant to print a dash
+#define DASH '-'
+
+// constant for end of c-string null character
+#define NEWLINE_CHAR '\n'
+
+// constant for small string length
+#define MIN_STR_LEN 32
+
+// constant for maximum string length
+#define MAX_STR_LEN 128
+
+// constant for end of c-string null character
+#define NULL_CHAR '\0'
+
+//Constant for printing a pipe
+#define PIPE '|'
+
+//Constant for a period
+#define PERIOD '.'
+
+//Constant for PRECISION
+#define PRECISION 2
+
+// constant for a SEMICOLON
+#define SEMICOLON ';'
+
+// constant for space character
+#define SPACE ' '
+
+// constant for standard string length
+#define STD_STR_LEN 64
+
+#endif  // STANDARD_CONSTANTS
---
+++ Sim04/Sim04_665911/datatypes.h
@@ -0,0 +1,92 @@
+// Preprocessor directives
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+// Data Types Used In Sim03
+
+// config data structure
+typedef struct ConfigDataType
+   {
+    double version;
+    char metaDataFileName[ 100 ];
+    int cpuSchedCode;
+    int quantumCycles;
+    int memAvailable;
+    int procCycleRate;
+    int ioCycleRate;
+    int logToCode;
+    char logToFileName[ 100 ];
+} ConfigDataType;
+
+// Memory Management Unit
+typedef struct MemoryDataType
+   {
+    // pid for parent Process
+    int pid;
+    // memory offset
+    int offset;
+    // memory base
+    int base;
+    // Base address register
+    int addressReg;
+    // pointer to next block of memory
+    struct MemoryDataType *nextBlock;
+} MemoryDataType;
+
+// Memory Management Unit
+typedef struct MMU
+   {
+    // Linked list of memory "blocks"
+    MemoryDataType *sysMemory;
+    // Total Memory Left
+    int memAvailable;
+    // total memory including Used
+    int memTotal;
+    // Number of processes using memory
+    int numProc;
+} MMU;
+
+typedef struct OpCodeType
+   {
+    int pid;             // pid, added when PCB is created
+    char command[ 5 ];   // three letter command quantity
+    char inOutArg[ 5 ];  // for device in/out
+    char strArg1[ 15 ];  // arg 1 descriptor, up to 12 chars
+    int intArg2;         // cycles or memory, assume 4 byte int
+    int intArg3;         // memory, assumes 5 byte int
+                         // also non/premption indicator
+    double opEndTime;    // size of time string returned from accessTimer
+    struct OpCodeType *nextNode; // pointer to next node as needded
+   } OpCodeType;
+
+// Data Structure used for every process
+// Every process has unique ID
+typedef struct ProcessType
+   {
+    int pid;                          // pid, added when PCB is created
+    int state;                        // process state, either NEW, READY,
+    int prevState;                    // RUNNING or EXITING.
+
+    int remainingTime;                // time remaining for process
+    OpCodeType *programCounter;       // pointer to start of process in op codes
+    struct ProcessType *nextProcess;  // pointer to next process
+   } ProcessType;
+
+typedef struct InterruptData
+   {
+    double blockedEndTime;             // End time for when the operation is
+                                       // finished and ready to interrupt
+    bool serviced;
+
+    ProcessType *process;       // pointer to start of process in op codes
+    struct InterruptData *nextInterrupt;  // pointer to next process
+} InterruptData;
+
+typedef struct OperationDataNode
+   {
+    char lineData[ 100 ];
+
+    struct OperationDataNode *nextNode;
+   } OperationDataNode;
+
+#endif  //DATATYPES_H
